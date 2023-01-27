ee75fc001896f8c90d0930936491a50a1b370147
shumwaynate <104692377+shumwaynate@users.noreply.github.com>
2023-01-27 13:48:43 -0700

    Added GUI Project for sprint 2

---
+++ GUIProject -Sprint2/GUIProject/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace GUIProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(45, 266);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(257, 266);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Choose a Shape!";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(11, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(59, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.Text = "Square";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(76, 19);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(74, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "Rectangle";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(156, 19);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(51, 17);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.Text = "Circle";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(86, 19);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(47, 17);
+            this.radioButton4.TabIndex = 6;
+            this.radioButton4.Text = "Area";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(11, 19);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(69, 17);
+            this.radioButton5.TabIndex = 7;
+            this.radioButton5.Text = "Perimeter";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Do you want Perimeter or Area?";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(151, 266);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 247);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Side Length";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(151, 246);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Side Width";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(257, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Circle Radius";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(45, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(206, 58);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Location = new System.Drawing.Point(45, 160);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(206, 50);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(103, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 65);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Calculate!!!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(283, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(189, 147);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Shape Calculator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ GUIProject -Sprint2/GUIProject/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace GUIProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public static int shape = 1;
+        public static int action = 1;
+        public static double length = 1;
+        public static double width = 1;
+        public static double radius = 1;
+        private void radioButton1_CheckedChanged(object sender, EventArgs e) //square
+        {
+            if (radioButton1.Enabled ==true || radioButton2.Enabled == true) { 
+                textBox1.Enabled = true;
+                textBox2.Enabled = false;
+                textBox2.Text = string.Empty;
+                textBox3.Enabled = false;
+                textBox3.Text = string.Empty;
+            }
+            if (radioButton1.Enabled == true)
+            {
+                pictureBox1.Image = Image.FromFile("../../Pictures/Square.png");
+            }
+            shape = 1;
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e) //Rectangle
+        {
+            if (radioButton2.Enabled == true) { 
+                textBox1.Enabled = true;
+                textBox2.Enabled = false;
+                textBox2.Text = string.Empty;
+                textBox3.Enabled = true;
+                pictureBox1.Image = Image.FromFile("../../Pictures/Rectangle.png");
+            }
+            shape = 2;
+        }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e) //Circle
+        {
+            if (radioButton3.Enabled == true) { 
+                textBox1.Enabled = false;
+                textBox1.Text = string.Empty;
+                textBox2.Enabled = true;
+                textBox3.Enabled = false;
+                textBox3.Text = string.Empty;
+                pictureBox1.Image = Image.FromFile("../../Pictures/Circle.png");
+            }
+            shape = 3;
+        }
+        private void radioButton5_CheckedChanged(object sender, EventArgs e) //Perimeter
+        {
+            action = 1;
+        }
+        private void radioButton4_CheckedChanged(object sender, EventArgs e) //Area
+        {
+            action = 2;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e) //side length
+        {
+            if (textBox1.Text != string.Empty)
+            {
+                length = Convert.ToDouble(textBox1.Text);
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e) //side width
+        {
+            if (textBox3.Text != string.Empty)
+            {
+                width = Convert.ToDouble(textBox3.Text);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e) //Circle radius
+        {
+            if (textBox2.Text != string.Empty)
+            {
+                radius = Convert.ToDouble(textBox2.Text);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Calculate
+        {
+            string answer = Calculate(length, width, radius, shape, action);
+            DialogResult dr = MessageBox.Show(answer, "Calculation Complete");
+        }
+        
+        public static string Calculate(double length, double width, double radius, int shape, int action)
+        { 
+            if(shape == 1){
+               if(action== 1){
+                   return String.Format("The Perimeter is {0,0:0.00}",getSquarePer(length));
+               }
+               else{
+                   return String.Format("The Area is {0,0:0.00}",getSquareArea(length));
+               }
+            }
+            else if (shape == 2){
+                if (action == 1){
+                    return String.Format("The Perimeter is {0,0:0.00}", getRectanglePer(width, length));
+                }
+                else{
+                    return String.Format("The Area is {0,0:0.00}", getRectangleArea(width, length));
+                }
+             }
+            else if (shape == 3){
+                if (action == 1){
+                    return "The Perimeter is "+ getCirclePer(radius);
+                }
+                else{
+                    return "The Area is " +getCircleArea(radius);
+                }
+            }
+            else { return "Something went terribly wrong!!!"; }
+          
+        }
+
+
+       public static double getSquarePer(double length){
+            return length * 4;
+        }
+        public static double getSquareArea(double length){
+            return length * length;
+        }
+        public static double getRectanglePer(double width, double height){
+            return (2 * width) + (2 * height);
+        }
+        public static double getRectangleArea(double width, double height){
+        return width * height;
+        }
+        public static String getCirclePer(double radius){
+            double dubRadius = radius * 2;
+
+
+            String perimeter = String.Format("{0, 0:0.00 }π", dubRadius);
+            return perimeter;
+        }
+        public static String getCircleArea(double radius){
+            double squRadius = radius * radius;
+            return String.Format("{0,0:0.00}π", squRadius);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e) //shape picture box
+        {
+            
+        }
+    }
+}
